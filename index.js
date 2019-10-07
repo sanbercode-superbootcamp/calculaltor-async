5d75882458045c94be2739cb21bf827080125e5e
Muhamad Hilmi Hibatullah <hilmihibatullah@gmail.com>
2019-10-07 10:09:05 +0700

    modulerize code

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const { add } = require('./calculator');
 const { expect } = require('chai');
 
 let result = 0;
-add(1, 2, function(err, res){
+
+function handleResult(err, res){
     expect(err).to.be.not.exist;
     expect(res).to.be.exist;
     expect(res).to.be.eq(3);
-});
+}
 
-add(1, null, function(err, res){
+function handleErrorResult(err, res){
     expect(err).to.be.exist;
     expect(res).to.be.not.exist;
-});
+}
+
+add(1, 2, handleResult);
+add(1, null, handleErrorResult);
