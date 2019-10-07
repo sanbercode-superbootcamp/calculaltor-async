30e52db4a7b91c6079d3bab6be18a54144cb9c6e
arifintahu <miftahul97@gmail.com>
2019-10-08 06:04:12 +0700

    pengumpulan tugas

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const { addPromise, ERROR_MSG } = require("./lib/calculator");
-const { expect } = require("chai");
+const { add, addPromise } = require('./calculator')
+const { expect } = require('chai')
 
-Promise.all([
-  addPromise(1,1, 3000),
-  addPromise(1,null, 1000)
-]).then(results => {
-}).catch(err => {
-  expect(err).to.be.exist;
-  expect(err.message).to.be.eq(ERROR_MSG);
-});
+function handleResult(err, res){
+    expect(err).to.be.not.exist
+    expect(res).to.be.exist
+    expect(res).to.be.eq(3)
+    console.log(res)
+}
+
+function handleErrorResult(err, res){
+    expect(err).to.be.exist
+    expect(res).to.be.not.exist
+}
+
+// add(1, 2, handleResult)
+// add(1, null, handleErrorResult)
+
+// addPromise(1, 2)
+//     .then((result) => addPromise(2, result))
+//     .then((result) => addPromise(3, result))
+//     .then((result) => addPromise(4, result))
+//     .then((result) => {
+//         console.log(result)
+//         expect(result).to.be.eq(12)
+//     })
+
+function printResult(result){
+    console.log(result)
+}
+
+addPromise(1, 2, 2000).then(printResult)
+addPromise(1, 1, 1000).then(printResult)
+
