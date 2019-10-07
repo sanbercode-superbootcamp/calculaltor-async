792e47e41a36a8da282a89c15ab6f1d76ba5d557
Hasan-Nabil <crispy.crunchy.creamy.chips@gmail.com>
2019-10-07 11:13:45 +0700

    menambahkan .mocharc.ymal dan spec nya

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const { addPromise } = require('./calculator');
+const { addPromise } = require('./lib/calculator');
 const { expect } = require('chai');
 
-// function handleResult (err, res){
-//     expect(err).to.be.not.exist;
-//     expect(res).to.be.exist;
-//     expect(res).to.be.eq(3)
-// }
+function handleResult (err, res){
+    expect(err).to.be.not.exist;
+    expect(res).to.be.exist;
+    expect(res).to.be.eq(3)
+}
 
-// function handleErrorResult (err, res){
-//     expect(err).to.be.exist;
-//     expect(res).to.be.not.exist;
-// }
+function handleErrorResult (err, res){
+    expect(err).to.be.exist;
+    expect(res).to.be.not.exist;
+}
 
-addPromise(1, 2)
-.then((result) => addPromise(2, result))
-.then((result) => addPromise(3, result))
-.then((result) => addPromise(4, result))
-.then((result) => {
+function printResult(result) {
     console.log(result);
-    expect(result).to.be.eq(12);
-});
+}
 
-addPromise(1, 1)
-.then((result) => addPromise(1, result))
-.then((result) => {
-    console.log(result);
-    expect(result).to.be.eq(3);
-});
+// addPromise(1, 2)
+// .then((result) => addPromise(2, result))
+// .then((result) => addPromise(3, result))
+// .then((result) => addPromise(4, result))
+// .then((result) => {
+//     console.log(result);
+//     expect(result).to.be.eq(12);
+// });
+
+// addPromise(1, 1)
+// .then((result) => addPromise(1, result))
+// .then((result) => {
+//     console.log(result);
+//     expect(result).to.be.eq(3);
+// });
+
+
+
 // let result = 0;
 // add(1,2, function(res) {
 //     result = res;
