6f6bd9e3aa3ac4508e0e722dc8ba5e85a762a1e6
ardhihdra <ardhi.rofi@gmail.com>
2019-10-07 14:12:20 +0700

    tambah unit test

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let result = 0;
 addPromise(1, 2)
   .then((result) => minusPromise(result,2))
   .then((result) => timesPromise(result,2))
-  .then((result) => dividePromise(result,2)
+  .then((result) => dividePromise(result,2))
   .then((result) => {
     console.log(result);
     expect(result).to.be.eq(1);
-  }));
+  });
 
 async function calculate(a,b) {
   result = await addPromise(a, b).then((result) => timesPromise(result, 100));
