c9a4e07248d0e77d0f31fe8e242644566b460e88
Muhamad Hilmi Hibatullah <hilmihibatullah@gmail.com>
2019-10-07 09:47:11 +0700

    change style function with callback

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
-function add(a,b) {
-    return a+b;
+function add(a,b, callback) {
+    const c = a + b;
+    callback(c);
 }
 
 exports.add = add;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { add } = require('./calculator');
 const { expect } = require('chai');
 
-const res = add(1,2);
-expect(res).to.be.eq(4);
+add(1, 2, function(res){
+    expect(res).to.be.eq(3);
+});
