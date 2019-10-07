3ab409adaf04de768b2f490a994f77374a7060b4
satriahdyt <satriahdyt@users.noreply.github.com>
2019-10-07 15:38:43 +0700

    nambahin testing

--- tugasCalc.spec.js
+++ tugasCalc.spec.js
@@ -1,4 +1,4 @@
-const { add, addPromise, ERROR_MSG } = require('./tugasCalc');
+const { add, addPromise, subtract, subtractPromise, times, timesPromise, divide, dividePromise, ERROR_MSG } = require('./tugasCalc');
 const { expect } = require('chai');
 const chai = require('chai');
 const chaiAsPromsied = require('chai-as-promised');
@@ -54,7 +54,7 @@ describe("calculator", function() {
             expect(result).to.be.eq(8)
         });
 
-        it.only("harusnya sih bisa gitu doang", async function() {
+        it("harusnya sih bisa penjumlahan doang", async function() {
             const result = await Promise.all([
                 addPromise(1, 2),
                 addPromise(9, 2),
@@ -66,6 +66,69 @@ describe("calculator", function() {
         context("input tydac valid", function() {
             it("harusnya keluar error ih", function() {
                 return expect(addPromise(null, 2))
+                    .to.be.rejectedWith(Error, ERROR_MSG);
+            });
+        });
+    });
+
+    describe("subtract promise", function() {
+        this.timeout(10000);
+        this.slow(10000);
+
+        it("harusnya sih bisa pengurangan doang", async function() {
+            const result = await Promise.all([
+                subtractPromise(10, 2),
+                subtractPromise(9, 2),
+                subtractPromise(17, -9)
+            ]);
+            expect(result).to.be.deep.eq([8, 7, 26])
+        });
+
+        context("input tydac valid", function() {
+            it("harusnya keluar error ye kan", function() {
+                return expect(subtractPromise(null, 2))
+                    .to.be.rejectedWith(Error, ERROR_MSG);
+            });
+        });
+    });
+
+    describe("times promise", function() {
+        this.timeout(10000);
+        this.slow(10000);
+
+        it("harusnya sih bisa perkalian doang", async function() {
+            const result = await Promise.all([
+                timesPromise(4, 2),
+                timesPromise(9, 2),
+                timesPromise(7, -9)
+            ]);
+            expect(result).to.be.deep.eq([8, 18, -63])
+        });
+
+        context("input tydac valid", function() {
+            it("harusnya keluar error ye kan", function() {
+                return expect(timesPromise(null, 2))
+                    .to.be.rejectedWith(Error, ERROR_MSG);
+            });
+        });
+    });
+
+    describe("divide promise", function() {
+        this.timeout(10000);
+        this.slow(10000);
+
+        it("harusnya sih bisa pembagian doang", async function() {
+            const result = await Promise.all([
+                dividePromise(10, 2),
+                dividePromise(9, 3),
+                dividePromise(1664, 4)
+            ]);
+            expect(result).to.be.deep.eq([5, 3, 416])
+        });
+
+        context("input tydac valid", function() {
+            it("harusnya keluar error ye kan", function() {
+                return expect(dividePromise(null, 2))
                     .to.be.rejectedWith(Error, ERROR_MSG);
             });
         });
