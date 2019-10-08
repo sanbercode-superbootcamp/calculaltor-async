8cdf439e9fd2edddb8dd4a24ce2340b9fd7c92fc
ardhihdra <ardhi.rofi@gmail.com>
2019-10-08 07:35:34 +0700

    perbaiki konflik

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { addPromise, minusPromise, timesPromise, dividePromise,  ERR_MSG } = require('./lib/calculator')
 const { expect } = require('chai');
 
@@ -7,16 +6,3 @@ exports.addPromise = addPromise;
 exports.minusPromise = minusPromise;
 exports.timesPromise = timesPromise;
 exports.dividePromise = dividePromise;
-=======
-const { addPromise, ERROR_MSG } = require("./lib/calculator");
-const { expect } = require("chai");
-
-Promise.all([
-  addPromise(1,1, 3000),
-  addPromise(1,null, 1000)
-]).then(results => {
-}).catch(err => {
-  expect(err).to.be.exist;
-  expect(err.message).to.be.eq(ERROR_MSG);
-});
->>>>>>> 5d9593f11984af76f7d4b73b04fe59fb6414c6f0
